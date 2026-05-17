0a0d919fd1c6621104f1cbb1d546a45547ea58a0
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-18 00:07:23 +0200

    feat: implement Prisma client and category fetching in OrderSidebar component

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,13 @@
+import { prisma } from '@/src/lib/prisma';
+
+const getCategories = async () => {
+  return await prisma.category.findMany();
+};
+
 const OrderSidebar = () => {
+  const categories = getCategories();
+  console.log(categories);
+
   return (
     <aside className="md:w-72 md:h-screen p-5 bg-white">OrderSidebar</aside>
   );
