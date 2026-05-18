050870e0e3d49f6226422500a5c7aa2e7f85e15a
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-18 22:18:00 +0200

    Remove unused SVG files and add new product images for burgers, cakes, coffee, cookies, and donuts.

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,15 +1,22 @@
 import { prisma } from '@/src/lib/prisma';
+import CategoryIcon from '../ui/CategoryIcon';
 
 const getCategories = async () => {
   return await prisma.category.findMany();
 };
 
-const OrderSidebar = () => {
-  const categories = getCategories();
-  console.log(categories);
+const OrderSidebar = async () => {
+  const categories = await getCategories();
 
   return (
-    <aside className="md:w-72 md:h-screen p-5 bg-white">OrderSidebar</aside>
+    <aside className="md:w-72 md:h-screen p-5 bg-white">
+      <h2 className="text-xl font-bold mb-4">Categories</h2>
+      <nav>
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} category={category} />
+        ))}
+      </nav>
+    </aside>
   );
 };
 
---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,26 @@
+import { Category } from '@/src/generated/prisma/client';
+import Image from 'next/image';
+
+type CategoryIconProps = {
+  category: Category;
+};
+
+const CategoryIcon = ({ category }: CategoryIconProps) => {
+  return (
+    <div
+      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b cursor-pointer hover:bg-gray-100 transition-colors`}
+    >
+      <div className="relative size-16">
+        <Image
+          src={`/icon_${category.slug}.svg`}
+          alt={`Image of category: ${category.name}`}
+          fill
+        />
+      </div>
+
+      <p className="text-lg font-medium">{category.name}</p>
+    </div>
+  );
+};
+
+export default CategoryIcon;
