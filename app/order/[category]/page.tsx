202d8ff2eae7b2e969f848b2b197f3bab7649062
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-18 22:55:32 +0200

    feat: update OrderPage to display category from params

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,5 +1,5 @@
-const OrderPage = () => {
-  return <div>OrderPage</div>;
+const OrderPage = ({ params }: { params: { category: string } }) => {
+  return <div>OrderPage: {params.category}</div>;
 };
 
 export default OrderPage;
