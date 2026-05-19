4523bb08ed108c781aaf0b3856f861e1200944f5
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-19 18:37:49 +0200

    feat: implement getProducts function to fetch products by category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,9 +1,25 @@
+import { prisma } from '@/src/lib/prisma';
+
+const getProducts = async (category: string) => {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+
+  return products;
+};
+
 const OrderPage = async ({
   params,
 }: {
   params: Promise<{ category: string }>;
 }) => {
   const { category } = await params;
+  const products = await getProducts(category);
+  console.log(products);
 
   return <div>OrderPage: {category}</div>;
 };
