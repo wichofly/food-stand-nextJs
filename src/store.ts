a471d5f17a072de06cdfc278a2940ee4f4ab3169
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-27 12:46:39 +0200

    feat: refactor Zustand store to improve order management logic in addToOrder function

--- src/store.ts
+++ src/store.ts
@@ -7,16 +7,32 @@ interface Store {
   addToOrder: (product: Product) => void;
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     const { categoryId, image, description, ...data } = product; // Exclude categoryId, image and description from the order item
 
-    set((state) => ({
-      order: [
-        ...state.order,
+    let order: OrderItem[] = [];
+
+    if (get().order.find((item) => item.id === data.id)) {
+      order = get().order.map((item) =>
+        item.id === data.id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+              subtotal: (item.quantity + 1) * product.price,
+            }
+          : item,
+      );
+    } else {
+      order = [
+        ...get().order,
         { ...data, quantity: 1, subtotal: 1 * product.price },
-      ],
+      ];
+    }
+
+    set(() => ({
+      order,
     }));
   },
 }));
