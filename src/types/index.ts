e71e576e47bc4b0d76dcc51762a753126bce6a4f
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-28 18:08:08 +0200

    feat: implement order management by adding OrderCard component and fetching pending orders

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
-import { Product } from '../generated/prisma/client';
+import { Order, OrderProducts, Product } from '../generated/prisma/client';
 
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
   quantity: number;
   subtotal: number;
+};
+
+export type OrderWithProducts = Order & {
+  orderProducts: (OrderProducts & {
+    product: Product;
+  })[];
 };
