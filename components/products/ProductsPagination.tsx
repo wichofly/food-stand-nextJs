fbb87551180ba486843de773a053945c47f6d719
Mauricio Erazo <antonierazo13@hotmail.com>
2026-05-29 21:53:40 +0200

    refactor: ProductsPagination to generate page links dynamically

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -6,6 +6,8 @@ type ProductsPaginationProps = {
 };
 
 const ProductsPagination = ({ page, totalPages }: ProductsPaginationProps) => {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
   return (
     <nav className="flex justify-center gap-2 py-5">
       {page > 1 && (
@@ -16,6 +18,18 @@ const ProductsPagination = ({ page, totalPages }: ProductsPaginationProps) => {
           &laquo;
         </Link>
       )}
+
+      {pages.map((currentPage) => (
+        <Link
+          key={currentPage}
+          className={`bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 ${
+            currentPage === page ? 'font-bold' : ''
+          }`}
+          href={`/admin/products?page=${currentPage}`}
+        >
+          {currentPage}
+        </Link>
+      ))}
 
       {page < totalPages && (
         <Link
