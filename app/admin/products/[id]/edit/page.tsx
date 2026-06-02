0291ccb15f4118f7bd4dff1750418edfd73a06a9
Mauricio Erazo <antonierazo13@hotmail.com>
2026-06-02 16:47:39 +0200

    feat: implement EditProductForm and update ProductForm for product editing
    - Automatically filled fields

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import EditProductForm from '@/components/products/EditProductForm';
+import ProductForm from '@/components/products/ProductForm';
+import Heading from '@/components/ui/Heading';
 import { prisma } from '@/src/lib/prisma';
 import { notFound } from 'next/navigation';
 
@@ -18,11 +21,19 @@ const EditProductsPage = async ({
 }: {
   params: Promise<{ id: string }>;
 }) => {
-  const { id } = await params; 
+  const { id } = await params;
   const product = await getProductById(+id); // Convert the id from string to number
 
   console.log(product);
-  return <div>EditProductsPage</div>;
+  return (
+    <>
+      <Heading>Edit Product: {product.name}</Heading>
+
+      <EditProductForm>
+        <ProductForm product={product} />
+      </EditProductForm>
+    </>
+  );
 };
 
 export default EditProductsPage;
---
+++ components/products/EditProductForm.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { createProduct } from '@/actions/create-product-action';
+import { ProductSchema } from '@/src/zod';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+
+const EditProductForm = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  const handleSubmit = async (formData: FormData) => {
+    const data = {
+      name: formData.get('name'),
+      description: formData.get('description'),
+      price: formData.get('price'),
+      categoryId: formData.get('categoryId'),
+      image: formData.get('image'),
+    };
+
+    const result = ProductSchema.safeParse(data);
+
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+
+      return;
+    }
+
+    const response = await createProduct(result.data);
+
+    if (response?.errors) {
+      response.errors.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+
+    toast.success('Product created successfully!');
+    router.push('/admin/products');
+  };
+
+  return (
+    <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
+      <form className="space-y-5" action={handleSubmit}>
+        {children}
+
+        <input
+          type="submit"
+          value="Save Changes"
+          className="bg-amber-500 text-white w-full mt-5 px-4 py-2 uppercase font-medium rounded-md hover:bg-amber-600 transition-colors cursor-pointer"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default EditProductForm;
