2130faf51dc6ff83e1ec8f057f2ee6d640c65500
Mauricio Erazo <antonierazo13@hotmail.com>
2026-06-02 20:28:18 +0200

    feat: add refresh functionality to OrdersPage for updating pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import OrderCard from '@/components/order/OrderCard';
 import Heading from '@/components/ui/Heading';
 import { prisma } from '@/src/lib/prisma';
+import { revalidatePath } from 'next/cache';
 
 const getPendingOrders = async () => {
   const orders = await prisma.order.findMany({
@@ -22,9 +23,25 @@ const getPendingOrders = async () => {
 const OrdersPage = async () => {
   const orders = await getPendingOrders();
 
+  const refreshOrders = async () => {
+    'use server';
+
+    revalidatePath('/admin/orders');
+  };
+
   return (
     <>
-      <Heading>Manage Orders</Heading>
+      <div className="flex flex-col lg:flex-row justify-between items-center">
+        <Heading>Manage Orders</Heading>
+
+        <form action={refreshOrders}>
+          <input
+            type="submit"
+            value="Update Orders"
+            className="bg-amber-500 text-white text-center w-full lg:w-auto text-xl px-10 py-3 font-medium rounded-md hover:bg-amber-600 transition cursor-pointer"
+          />
+        </form>
+      </div>
 
       {orders.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
