6f339d6a0d4a392a91d6c8ebbc35f88a5715e230
Mauricio Erazo <antonierazo13@hotmail.com>
2026-06-02 19:54:27 +0200

    fix: update grid layout for product display on OrderPage

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -28,7 +28,7 @@ const OrderPage = async ({
     <>
       <Heading>Menu: {formattedCategory}</Heading>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 gap-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
