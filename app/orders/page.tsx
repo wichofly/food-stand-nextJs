a668ea3caa7e58658a19c1236b0cb7256333179d
Mauricio Erazo <antonierazo13@hotmail.com>
2026-06-07 18:47:58 +0200

    feat: add Orders API and create OrdersPage component for displaying ready orders

---
+++ app/orders/page.tsx
@@ -0,0 +1,13 @@
+import Logo from '@/components/ui/Logo';
+
+const OrdersPage = () => {
+  return (
+    <>
+      <h1 className="text-center mt-10 text-6xl font-semibold">Ready Orders</h1>
+
+      <Logo />
+    </>
+  );
+};
+
+export default OrdersPage;
