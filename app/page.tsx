0dbff7a32ce4ad60831b5661c8fe63255d55aa8e
Mauricio Erazo <antonierazo13@hotmail.com>
2026-06-07 21:41:08 +0200

    feat: redirect Home component to /order/coffee

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-semibold p-5">Welcome to the Food Stand!</h1>
-  );
+  redirect('/order/coffee');
 }
